e6eec593c745bfdea72579a0cf13a3c3a245100c
Kenny Home <lizneth72@yahoo.com>
2021-07-25 17:35:21 -0400

    Added Scripts/basic_netmiko.py file.

---
+++ Scripts/basic_netmiko.py
@@ -0,0 +1,37 @@
+from netmiko import ConnectHandler
+
+cisco_username = input("Enter Username: ")
+cisco_password = input("Enter Password: ")
+
+R1 = {
+    'device_type': 'cisco_ios',
+    'host':   '192.168.177.101',
+    'username': cisco_username,
+    'password': cisco_password,
+}
+
+R2 = {
+    'device_type': 'cisco_ios',
+    'host':   '192.168.177.102',
+    'username': cisco_username,
+    'password': cisco_password,
+}
+
+R3 = {
+    'device_type': 'cisco_ios',
+    'host':   '192.168.177.103',
+    'username': cisco_username,
+    'password': cisco_password,
+}
+
+
+my_routers = [R1, R2, R3]
+
+
+for router in my_routers:
+    net_connect = ConnectHandler(**router)
+    output = net_connect.send_config_from_file("cisco_commands.txt")
+    print("!#****************************\n")
+    print(output)
+
+
