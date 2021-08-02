39796eed8fa8b84f95c3886a1c7464bf74652e02
Samaniego <samaniegk@leidos.com>
2021-08-02 10:08:22 -0400

    Deleted unused variables.

--- batfish_scripts/consolidate_csv.py
+++ batfish_scripts/consolidate_csv.py
@@ -2,9 +2,6 @@ import os
 import pandas as pd
 import glob
 
-path = './'
-#all_files = glob.glob(os.path.join(path, "*.csv", recursive=True))
-#all_files = glob.glob(os.path.join(path, "*.csv", recursive=True))
 all_files = glob.glob(f".\output\**\*.csv", recursive=True)
 
 writer = pd.ExcelWriter('Consolidated_Output.xlsx')
