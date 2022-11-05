8afbd5958734018a530a00a16c36a1ef3dee8482
Kenny <lizneth72@yahoo.com>
2022-11-05 16:33:16 -0400

    Added comments and spacing for clarity.

--- working_dir/nornir_group_tasks.py
+++ working_dir/nornir_group_tasks.py
@@ -11,37 +11,36 @@ def count(task: Task, number: int) -> Result:
 def say(task: Task, text:str) -> Result:
     return Result(
         host = task.host,
-        result = f"(task.host.name) says {text}"
+        result = f"{task.host.name} says {text}"
     )
 
 def greet_and_count(task: Task, number: int) -> Result:
     task.run(
-        name = "Greeting is the polite thing to do.",
+        #name = "Greeting is the polite thing to do.",
         task = say,
         text = "hi!"
     )
     task.run(
-        name = "Counting beans.",
+        #name = "Counting beans.",
         task = count,
         number = number
     )
-
     task.run(
-        name = "We should say bye too.",
+        #name = "We should say bye too.",
         task = say,
-        text = "Bye!"
+        text = "Bye!\n\n"
     )
-
-    even_or_odds = "even" if number %2 == 1 else "odd"
+    even_or_odds = "odd" if number %2 == 1 else "even"
     return Result(
-       host = task.host,
-       result = f"{task.host} counted {even_or_odds} times!" 
+        host = task.host,
+        result = f"{task.host.name} counted {even_or_odds} times!" 
     )
 
 
 nr = InitNornir(config_file="config.yaml")
 
 result = nr.run(
+    name = "TOP OF FUNCTION.",
     task = greet_and_count,
     number = 5
 )
--- working_dir/nornir_task.py
+++ working_dir/nornir_task.py
@@ -7,10 +7,10 @@ nr = InitNornir(config_file="config.yaml")
 # A task is a reusable piece of code that implements some functionality for a single host.
 # In python terms it is a function that takes a Task as first paramater and returns a Result.
 
-def say(task: Task, text: str) -> Result:
+def say(task: Task, text: str, text2: str) -> Result:
     return Result(
         host = task.host,
-        result = f"{task.host.name} says {text}"
+        result = f"{task.host.name} says {text} and {text2}"
     )
 
 # Tasks can also take any number of arguments to extend their functionality.
@@ -19,7 +19,8 @@ result = nr.run(
     # The "name" argument gives the task a descriptive name.
     name = "Kids holiday",
     task = say,
-    text = "Happy Halloween!"
+    text = "Happy Halloween",
+    text2 = "Bye!"
 )
 
 print_result(result)
