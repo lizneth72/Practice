0b12c84b57fe85fbd0057336e607217f9765b173
Kenny <lizneth72@yahoo.com>
2022-11-05 15:34:41 -0400

    Fixed syntax errors.

--- working_dir/nornir_group_tasks.py
+++ working_dir/nornir_group_tasks.py
@@ -1,4 +1,6 @@
 from nornir import InitNornir
+from nornir.core.task import Task, Result
+from nornir_utils.plugins.functions import print_result
 
 def count(task: Task, number: int) -> Result:
     return Result(
@@ -6,7 +8,7 @@ def count(task: Task, number: int) -> Result:
         result = f"{[n for n in range (0, number)]}"
     )
 
-def say(task" Task, text:str) -> Result:
+def say(task: Task, text:str) -> Result:
     return Result(
         host = task.host,
         result = f"(task.host.name) says {text}"
@@ -14,26 +16,34 @@ def say(task" Task, text:str) -> Result:
 
 def greet_and_count(task: Task, number: int) -> Result:
     task.run(
-        name = "Greeting is the polite thing to do."
-        task = task.host,
+        name = "Greeting is the polite thing to do.",
+        task = say,
         text = "hi!"
     )
     task.run(
-        name = "Counting beans."
-        task = count
+        name = "Counting beans.",
+        task = count,
         number = number
     )
 
     task.run(
-        name = "We should say bye too."
+        name = "We should say bye too.",
         task = say,
         text = "Bye!"
     )
 
     even_or_odds = "even" if number %2 == 1 else "odd"
     return Result(
-       host = task.host
+       host = task.host,
        result = f"{task.host} counted {even_or_odds} times!" 
     )
+
+
+nr = InitNornir(config_file="config.yaml")
+
+result = nr.run(
+    task = greet_and_count,
+    number = 5
+)
 
 print_result(result)
